4c728571b6fab02846a91e6135e0e6dcfc756935
Donald Pakkies <donaldpakkies@gmail.com>
2022-03-19 13:48:53 +0000

    feat: build imba and ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,5 +4,6 @@ import { Form } from "./Form";
 import { hasAttr } from "./hasAttr";
 import { isWaiting } from "./isWaiting";
 import { readProps } from "./readProps";
+import { rescue } from "./rescue";
 import { useForm } from "./useForm";
-export { clone, csrf, Form, hasAttr, isWaiting, readProps, useForm };
+export { clone, csrf, Form, hasAttr, isWaiting, readProps, rescue, useForm };
