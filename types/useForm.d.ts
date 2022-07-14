7e11e465aedd9295fa939f3e28d4dbc1c03becdf
Donald Pakkies <donaldpakkies@gmail.com>
2022-07-14 16:59:57 +0200

    feat: build imba and compile typescript

--- types/useForm.d.ts
+++ types/useForm.d.ts
@@ -1,2 +1,6 @@
-export function useForm(body?: {}, config?: {}): Form;
+/**
+@param {FormConfig} config
+*/
+export function useForm(body?: {}, config?: FormConfig): Form;
 import { Form } from "./Form";
+import type { FormConfig } from "../ts";
